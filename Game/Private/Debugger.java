6c46ba33735f789d7df5d4c22a5147f997e84a4c
Muhammad Quwais Safutra <44664132+TrojanIsHere@users.noreply.github.com>
2018-12-17 21:19:58 +0700

    Create Debugger.java

---
+++ Game/Private/Debugger.java
@@ -0,0 +1,5 @@
+/* WRITE BY : AHMAD YUSUF KALLA */
+
+HashMap<Integer, Integer> record = new HashMap<Integer, Integer>();
+        for(int i = 0 ; i < nums.length ; i ++)
+            record.put(nums[i], i);
